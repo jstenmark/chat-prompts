1cc854d416f16dfa6ed0e3f5d21b45f70f1498ae
JStenmark <johannes@stenmark.in>
2023-10-31 15:47:07 +0100

    over in west Philadelphia a puppy is vomiting ...

--- utils/commit_util.sh
+++ utils/commit_util.sh
@@ -8,8 +8,9 @@ git status
 if [ "$1" == "yolo" ]; then
   COMMIT_MSG=$(fortune 2>/dev/null) || COMMIT_MSG="okaayyy-lets-go"
   CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
-
-  echo "\r\nCOMMIT_MSG=$COMMIT_MSG\r\n"
+  echo "---------"
+  echo "$COMMIT_MSG"
+  echo "---------"
   git commit -m "$COMMIT_MSG"
   git push origin "$CURRENT_BRANCH" --force
 fi
