ecf524ec9df8e1e88b35ab6088497b97a624bb55
JStenmark <johannes@stenmark.in>
2023-10-31 15:42:54 +0100

    Holiday and other blackout periods apply.

--- utils/commit_util.sh
+++ utils/commit_util.sh
@@ -3,3 +3,14 @@
 git add -A
 git reset HEAD *.md
 git status
+
+# Checking if "yolo" argument is provided
+if [ "$1" == "yolo" ]; then
+  COMMIT_MSG=$(fortune 2>/dev/null) || COMMIT_MSG="okaayyy-lets-go"
+  echo "Committing with message: $COMMIT_MSG"
+
+  git commit -m "$COMMIT_MSG"
+
+  CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+  git push origin "$CURRENT_BRANCH" --force
+fi
