c242b65a2afb2a02a65f644c96c12d6a64d2cfb8
JStenmark <johannes@stenmark.in>
2023-10-31 15:46:01 +0100

    The "cutting edge" is getting rather dull. 		-- Andy Purshottam

--- utils/commit_util.sh
+++ utils/commit_util.sh
@@ -7,10 +7,9 @@ git status
 # Checking if "yolo" argument is provided
 if [ "$1" == "yolo" ]; then
   COMMIT_MSG=$(fortune 2>/dev/null) || COMMIT_MSG="okaayyy-lets-go"
-  echo "Committing with message: $COMMIT_MSG"
-
-  git commit -m "$COMMIT_MSG"
-
   CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+
+  echo "\r\nCOMMIT_MSG=$COMMIT_MSG\r\n"
+  git commit -m "$COMMIT_MSG"
   git push origin "$CURRENT_BRANCH" --force
 fi
